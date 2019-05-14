71db7b1b61b1cf5b6b933d96a36e88255c430904
Kirill <AnDrOlEiN@MacBook-Pro.local>
2019-05-14 19:47:39 +0300

    makefile changes

--- make-app.mk
+++ make-app.mk
@@ -1,10 +1,8 @@
-USER = nobody
 
 app:
 	docker-compose up
 
 app-setup: ans-development-setup-env app-build
-	docker-compose run --user=${USER} app make install
 
 app-build:
 	docker-compose build
