2082454907558cfdd753103f0a9ca6140788d497
Kiryl Surahatau <androlein@gmail.com>
2019-05-15 10:14:41 +0300

    tfstate added as vault

--- make-ans.mk
+++ make-ans.mk
@@ -5,10 +5,10 @@ ans-get-docker-role:
 	ansible-galaxy install nickjj.docker
 
 ans-vaults-encrypt:
-	ansible-vault encrypt ansible/production/group_vars/all/vault.yml ansible/group_vars/all/vault.yml
+	ansible-vault encrypt ansible/production/group_vars/all/vault.yml ansible/group_vars/all/vault.yml terraform.tfstate
 
 ans-vaults-decrypt:
-	ansible-vault decrypt ansible/production/group_vars/all/vault.yml ansible/group_vars/all/vault.yml
+	ansible-vault decrypt ansible/production/group_vars/all/vault.yml ansible/group_vars/all/vault.yml terraform.tfstate
 
 ans-trf-vars:
 	ansible-playbook ansible/terraform.yml -i ansible/development --ask-vault-pass
